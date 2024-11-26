90344045cea234ca3fc528778b92539b8799d65f
Lykoros <lykoropro@gmail.com>
2024-11-26 17:54:10 +0100

    finished namespaces

--- include/namespace.hpp
+++ include/namespace.hpp
@@ -1,12 +1,27 @@
 #ifndef __NAMESPACE__
   #define __NAMESPACE__
+  #include <iostream>
 
 
   namespace Namespace //any name except reserved one
   {
     void test(); // -> namespace::test()
   }
-
     /*void test(); -> test()*/
+  
+  namespace TooLongNamespaceName
+  {
+    void test();
+  }
+  namespace TLNSN = TooLongNamespaceName; //Alias, same as using TLNSN = TooLongNamespaceName but using is for everything -> using char = int to make char i = 2;
+
+//anonymous namespace
+  namespace
+  {
+    void sex()
+    {
+      std::cout << "hello" << std::endl; //Cannot acces outside this file (and linked)
+    }
+  }
 
 #endif // !__
--- src/namespace.cpp
+++ src/namespace.cpp
@@ -7,7 +7,19 @@ void Namespace::test()
   std::cout << "test" << std::endl;
 }
 
+void TLNSN::test()
+{
+  std::cout << "test2" << std::endl;
+}
+
 void nameSpace()
 {
   Namespace::test();
+  TLNSN::test();
+  sex();
+
+  {
+    using namespace Namespace; //Will simulate the Namespace::
+    test();
+  }
 }
