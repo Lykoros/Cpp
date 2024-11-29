4bee3937687f1c315ce598e18bbe3ef17399f39a
Lykoros <lykoropro@gmail.com>
2024-11-29 20:25:34 +0100

    added native build Inputs

--- default.nix
+++ default.nix
@@ -1,12 +1,19 @@
 {
 lib,
-stdenv
+stdenv,
+gnumake,
+gcc
 }:
 
 stdenv.mkDerivation rec {
   name = "cppProg";
 
   src = ./.;
+
+  nativeBuildInputs = [
+    gnumake
+    gcc
+  ];
 
   installPhase = ''
     mkdir -p $out/bin
